a988633d17cb3af7530e6689d9521fb35d67518f
Ashwin <ashwinkey04@gmail.com>
2020-09-04 20:11:33 +0530

    Initial commit

---
+++ app/src/main/java/com/ashwinkey04/fit7/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ashwinkey04.fit7
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.ashwinkey04.fit7.ui.Fit7Theme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Fit7Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Fit7Theme {
+        Greeting("Android")
+    }
+}
