b0ac9196f4ec313c3bcb19d4edd152fb299979c5
Ashwin <ashwinkey04@gmail.com>
2020-09-07 22:14:04 +0530

    feat: Add app base theme composable

--- app/src/main/java/com/ashwinkey04/fit7/MainActivity.kt
+++ app/src/main/java/com/ashwinkey04/fit7/MainActivity.kt
@@ -3,10 +3,15 @@ package com.ashwinkey04.fit7
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.ashwinkey04.fit7.ui.Fit7Theme
 
@@ -16,23 +21,39 @@ class MainActivity : AppCompatActivity() {
         setContent {
             Fit7Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                SampleScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Fit7Base(content: @Composable () -> Unit) {
+    Fit7Theme {
+        Surface(color = Color.Gray) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(16.dp))
+}
+
+@Preview("Something")
 @Composable
 fun DefaultPreview() {
-    Fit7Theme {
-        Greeting("Android")
+    Fit7Base {
+        SampleScreen()
+    }
+}
+
+@Composable
+fun SampleScreen() {
+    Column {
+        Greeting("Ashwin")
+        Divider(color = Color.Black)
+        Greeting("there")
     }
 }
